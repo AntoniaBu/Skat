1b6c7e79519c876831defdc26b89290665dbce05
AntoniaBu <antonia.j.busch@gmail.com>
2022-05-09 17:33:54 +0200

    Add Console class and some methods to Card, Deck and Player class

--- Skat.py
+++ Skat.py
@@ -27,14 +27,50 @@ class Card:
     
     def get_suit(self):
         return self.suit
-               
+      
+    # how name of the card should be shown         
     def get_name(self):
-        # how name of the card should be shown
         return self.get_suit() + ' ' + self.get_rank()
     
+    # how to print a cards name
     def __repr__(self):
-        # how to print a cards name
         return self.get_name()
+    
+    # define int value for the cards to make them sortable
+    def num_rank(self, rank):
+        if rank == "Bube":
+            return 14
+        if rank == "Ass":
+            return 13
+        if rank == "10":
+            return 12
+        if rank == "König":
+            return 11
+        if rank == "Dame":
+            return 10
+        return int(rank)
+    
+    def num_suit(self, suit):
+        if suit == "Karo":
+            return 1
+        if suit == "Herz":
+            return 2
+        if suit == "Pik":
+            return 3
+        if suit == "Kreuz":
+            return 4
+        return int(suit)
+    
+    def __lt__(self, o):     # tuple ordering
+        c1 = self.num_suit(self.suit), self.num_rank(self.rank)
+        c2 = o.num_suit(o.suit), o.num_rank(o.rank)   
+        return c1 < c2
+
+    def __eq__(self, o):
+        c1 = self.num_suit(self.suit), self.num_rank(self.rank)
+        c2 = o.num_suit(o.suit), o.num_rank(o.rank)
+        return c1 == c2
+    
 
 class Deck:
     
@@ -45,15 +81,11 @@ class Deck:
         ranks = ['Ass', '10', 'König', 'Dame', 'Bube', '9', '8', '7']
         suits = ['Kreuz', 'Pik', 'Herz', 'Karo']
         
-        for suit in suits:
-            for rank in ranks:
-                # create full 32 cards deck
-                self.cards_in_deck.append(Card(rank, suit))
-      
-        
+        # create full 32 cards deck
+        self.cards_in_deck = [Card(r,s) for r in ranks for s in suits]
+          
     def get_size(self):
-        return len(self.cards_in_deck)
-    
+        return len(self.cards_in_deck)  
    
     def deal_cards(self, num):
         # deals as many cards as specified by 'num'
@@ -61,12 +93,14 @@ class Deck:
             self.dealt_cards = []
         for i in range(num):
             self.dealt_cards.append(self.cards_in_deck.pop())
-        return self.dealt_cards
-    
+        return self.dealt_cards    
     
     def shuffle(self):
         # shuffles cards in the deck
         return random.shuffle(self.cards_in_deck)
+    
+    def order(self):
+        self.cards_in_deck.sort()
 
 
 class Player:
@@ -77,6 +111,12 @@ class Player:
                 
     def take_cards(self, hand_cards):
         self.hand.extend(hand_cards)
+      
+    def sort_cards(self):
+        return self.hand.sort()
+        
+    def play_card(self):
+        pass
         
        
 
@@ -92,52 +132,79 @@ class Game:
         return(p1, p2, p3)
     
 
+class Console:
+    
+    def init(self):
+        pass
+    
+    def type_player_names(self):
+        x = input("Name player 1: ")
+        y = input("Name player 2: ")
+        z = input("Name player 3: ")
+        return(x, y, z)
+    
+    def select_card(self, hand_cards):
+        card = input("\nType the card you want to play: ")
+        
+# =============================================================================
+#         while True:
+#             try:
+#                 card in hand_cards
+#                 break
+#             except ValueError:
+#                 print('Please select a hand card.')
+#                 
+#         print('\n' + card + ' was played.')     
+# =============================================================================
+        return card
 
        
 if __name__ == "__main__":
     
-  
-# =============================================================================
-#     x = input("Name player 1: ")
-#     y = input("Name player 2: ")
-#     z = input("Name player 3: ")
-# =============================================================================
     
     x = 'x'
     y = 'y'
     z = 'z'
 
-    g = Game()
-    player1, player2, player3 = g.create_players(x, y, z)
+    c = Console()
+    #x, y, z = c.get_player_names()
     
+    # create game
+    g = Game()
+    # create players
+    player1, player2, player3 = g.create_players(x, y, z)
+    # create deck
     d = Deck()
+    # shuffle deck
     d.shuffle()
     
-    player1.take_cards(d.deal_cards(8))
-    print(d.get_size())
-    print(player1.hand)
-    
-    player2.take_cards(d.deal_cards(3))
-    print(d.get_size())
-    print(player2.hand)
-    
-    player3.take_cards(d.deal_cards(4))
-    print(d.get_size())
-    print(player3.hand)
-    
-    
 # =============================================================================
-#     print(player1.hand)
-#     print(len(player1.hand)) 
-#     print(player2.hand)
-#     print(len(player2.hand))
-#     print(player3.hand)
-#     print(len(player3.hand))
+#     d.cards_in_deck.sort()
+#     print(d.cards_in_deck)
 # =============================================================================
 
-   
-    #print(d.deal_cards(10))
+    #players take 10 cards each
+    player1.take_cards(d.deal_cards(10))
+    player2.take_cards(d.deal_cards(10))
+    player3.take_cards(d.deal_cards(10))
     
+    #players sort their cards
+    player1.sort_cards()
+    player2.sort_cards()
+    player3.sort_cards()
+    
+    print('\nPlayer 1 cards: ')
+    print(player1.hand)
+    
+    # player selects a card from their hand they want to play
+    # card is removed from their hand
+    card_played = c.select_card(player1.hand)
+    
+    #player1.hand.remove(card_played)
+
+    print('\nPlayer 1 cards: ')
+    print(player1.hand)
+
 
 
 # =============================================================================
